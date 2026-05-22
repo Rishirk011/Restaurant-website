6bec0f42e2896d1afbda8480f4c3cc2bbeebd318
rishi <rishirk253@gmail.com>
2026-05-22 19:00:49 +0530

    fixing a image related bug

--- src/components/data/data.ts
+++ src/components/data/data.ts
@@ -8,15 +8,15 @@ export interface foodItemListType{
 
 export const foodItemList:foodItemListType[]=[
     
-    {id:1,name:"Mc veggie",price:225,img:"/public/food/one.jpeg",quantity:1},
+    {id:1,name:"Mc veggie",price:225,img:"/food/one.jpeg",quantity:1},
     
-    {id:2,name:"Fish Burger",price:250,img:"/public/food/12.jpeg",quantity:1},
+    {id:2,name:"Fish Burger",price:250,img:"/food/12.jpeg",quantity:1},
     
-    {id:3,name:"Kebab Burger",price:200,img:"/public/food/six.jpeg",quantity:1},
+    {id:3,name:"Kebab Burger",price:200,img:"/food/six.jpeg",quantity:1},
     
-    {id:4,name:"Whooper",price:150,img:"/public/food/one.jpeg",quantity:1},
+    {id:4,name:"Whooper",price:150,img:"/food/one.jpeg",quantity:1},
 
-    {id:5,name:"Mcaalo tikki",price:30,img:"/public/food/nine.jpeg",quantity:1},
+    {id:5,name:"McAalo tikki",price:30,img:"/food/nine.jpeg",quantity:1},
 
     {id:6,name:"Juicy lucy",price:200,img:"/public/food/12.jpeg",quantity:1},
 
